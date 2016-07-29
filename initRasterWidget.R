a51d02e0aa9bd6cf05cee872405b319d8a5271c9
Christopher Stephan <c.stephan@wwu.de>
2016-07-29 19:25:52 +0200

    Added roxygen2 to initialization.

--- initRasterWidget.R
+++ initRasterWidget.R
@@ -3,5 +3,7 @@ install.packages("devtools")
 install.packages("raster")
 install.packages("classInt")
 install.packages("rgdal")
+install.packages("roxygen2")
 
 use_travis('/Users/christopherstephan/Documents/git/rasterWidget/rasterWidget')
+devtools::document()
