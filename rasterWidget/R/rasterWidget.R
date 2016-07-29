cf876282c2c6c024a2e530e80e28209f6d78dc21
Christopher Stephan <c.stephan@wwu.de>
2016-07-29 19:20:59 +0200

    Fixed imports and documentations.

--- rasterWidget/R/rasterWidget.R
+++ rasterWidget/R/rasterWidget.R
@@ -5,11 +5,15 @@
 #' raster object to inspect its cell values when hovering over them.
 #'
 #' @import htmlwidgets
+#' @import raster
+#' @import classInt
+#' @import stats
+#' 
 #' @param raster a \code{Raster*} or \code{Spatial*} object.
 #' @param width width of the widget in px
 #' @param height height of the widget in px
 #' @param nclass number of classes required 
-#' @param stlye chosen style: one of "fixed", "sd", "equal", "pretty", "quantile", "kmeans", "hclust", "bclust", "fisher", or "jenks"
+#' @param style chosen style: one of "fixed", "sd", "equal", "pretty", "quantile", "kmeans", "hclust", "bclust", "fisher", or "jenks"
 #' @param colors a character vector for color coding the class intervals
 #' @param colNA character for the color of NA values
 #' 
@@ -49,6 +53,9 @@ rasterWidget <- function(raster, width = 400, height = 400, nclass, style="fishe
 
 #' Widget output function for use in Shiny
 #'
+#' @param outputId outputId
+#' @param width width in %
+#' @param height height in px
 #' @export
 rasterWidgetOutput <- function(outputId, width = '100%', height = '400px'){
   shinyWidgetOutput(outputId, 'rasterWidget', width, height, package = 'rasterWidget')
@@ -56,6 +63,9 @@ rasterWidgetOutput <- function(outputId, width = '100%', height = '400px'){
 
 #' Widget render function for use in Shiny
 #'
+#' @param expr expr
+#' @param env env
+#' @param quoted quoted
 #' @export
 renderRasterWidget <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
